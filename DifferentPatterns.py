354919417d46c9f182a1c465d5a2fe8f71bb83f0
Ajay Pradeep <79718383+ajaypradeep-mahindan@users.noreply.github.com>
2022-06-14 12:34:39 +0530

    Add files via upload

---
+++ DifferentPatterns.py
@@ -0,0 +1,24 @@
+n=int(input('Enter the no of times to printed:'))
+for i in range(0,n+1): #to determine rows
+    for j in range(1,i+1): #to give numbers
+        print(i,end=" ")
+    print()      
+    
+    
+n=int(input('Enter the no of times to printed:'))
+for i in range(0,n+1): #to determine rows
+    for j in range(1,i+1): #to give numbers
+        print(j,end=" ")
+    print()      
+    
+n=int(input('Enter the no of times printed:'))
+for i in range(0,n+1): #to determine rows
+    for j in range(1,i+1): #to give numbers
+        print(n,end=" ")
+    print()  
+    
+n=int(input('Enter the no of times to printed:'))
+for i in range(0,n+1): #to determine rows
+    for j in range(1,i+1): #to give numbers
+        print('*',end=" ")
+    print()                          
